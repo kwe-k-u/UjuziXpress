09dfe81ee7f5c0147edd6db6e30d4d5b6b47a5cc
Kweku <kwekuappiah11@gmail.com>
2021-03-20 15:41:23 +0000

    added background image to login and sign up

--- lib/UI/screens/SignupPage.dart
+++ lib/UI/screens/SignupPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ujuzi_xpress/UI/screens/HomePage.dart';
 import 'package:ujuzi_xpress/UI/screens/LoginPage.dart';
+import 'package:ujuzi_xpress/UI/widgets/BackgroundWidget.dart';
 import 'package:ujuzi_xpress/UI/widgets/CustomIconButton.dart';
 import 'package:ujuzi_xpress/UI/widgets/CustomImageButton.dart';
 import 'package:ujuzi_xpress/UI/widgets/CustomTextButton.dart';
@@ -19,113 +20,115 @@ class _SignupPageState extends State<SignupPage> {
 
 
     return Scaffold(
-      backgroundColor: Colors.white70,
+      backgroundColor: Colors.white.withOpacity(0.85),
       body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.only(left: 20.0, top: size.height * 0.08, right: 8.0, bottom: size.height * 0.08),
-          width: size.width,
-          height: size.height,
+        child: BackgroundWidget(
+          child: Container(
+            padding: EdgeInsets.only(left: 20.0, top: size.height * 0.08, right: 8.0, bottom: size.height * 0.08),
+            width: size.width,
+            height: size.height,
 
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
 
-              Spacer(flex: 1,),
+                Spacer(flex: 1,),
 
-              Row(
-                children: [
-                  Text("SIGN UP")
-                ],
-              ),
-
-
-              Spacer(flex: 1,),
-              Center(child: Text("SIGN UP WITH", )),
-
-              ButtonBar(
-                alignment: MainAxisAlignment.spaceAround,
-                children: [
-                  CustomImageButton(
-                    path: "assets/google_logo.png",
-                    onPressed: (){
-
-                    },
-                  ),
-
-                  CustomImageButton(
-                    path: "assets/facebook_logo.png",
-                    onPressed: (){
-
-                    },
-                  ),
-
-                  CustomImageButton(
-                    path: "assets/twitter_logo.png",
-                    widthFactor: 0.14,
-                    onPressed: (){
-
-                    },
-                  ),
-                ],
-              ),
-
-              //Name
-              CustomTextField(
-                label: "Name",
-                inputType: TextInputType.name,
-              ),
-
-              //Email
-              CustomTextField(
-                label: "Email",
-                inputType: TextInputType.emailAddress,
-              ),
-
-              //Password
-              CustomTextField(
-                label: "Password",
-                obscureText: true,
-              ),
-
-              //number
-              CustomTextField(
-                label: "Phone number",
-                inputType: TextInputType.phone,
-              ),
-
-
-              Spacer(flex: 1,),
-
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: CustomTextButton(
-                  actionText: "Login",
-                  onPressed: (){
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context)=> LoginPage())
-                    );
-                  },
+                Row(
+                  children: [
+                    Text("SIGN UP")
+                  ],
                 ),
-              ),
 
 
-              Padding(
-                padding: const EdgeInsets.only(right:8.0, top: 12.0, bottom: 16.0),
-                child: Align(
-                  alignment: Alignment.centerRight,
-                  child: CustomIconButton(
-                    color: Colors.purple,
+                Spacer(flex: 1,),
+                Center(child: Text("SIGN UP WITH", )),
+
+                ButtonBar(
+                  alignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    CustomImageButton(
+                      path: "assets/google_logo.png",
+                      onPressed: (){
+
+                      },
+                    ),
+
+                    CustomImageButton(
+                      path: "assets/facebook_logo.png",
+                      onPressed: (){
+
+                      },
+                    ),
+
+                    CustomImageButton(
+                      path: "assets/twitter_logo.png",
+                      widthFactor: 0.14,
+                      onPressed: (){
+
+                      },
+                    ),
+                  ],
+                ),
+
+                //Name
+                CustomTextField(
+                  label: "Name",
+                  inputType: TextInputType.name,
+                ),
+
+                //Email
+                CustomTextField(
+                  label: "Email",
+                  inputType: TextInputType.emailAddress,
+                ),
+
+                //Password
+                CustomTextField(
+                  label: "Password",
+                  obscureText: true,
+                ),
+
+                //number
+                CustomTextField(
+                  label: "Phone number",
+                  inputType: TextInputType.phone,
+                ),
+
+
+                Spacer(flex: 1,),
+
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CustomTextButton(
+                    actionText: "Login",
                     onPressed: (){
-                      //todo authenticate
-                      Navigator.pushReplacement(context, MaterialPageRoute(
-                        builder: (context)=> HomePage()
-                      ));
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context)=> LoginPage())
+                      );
                     },
                   ),
                 ),
-              )
-            ],
+
+
+                Padding(
+                  padding: const EdgeInsets.only(right:8.0, top: 12.0, bottom: 16.0),
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: CustomIconButton(
+                      color: Colors.purple,
+                      onPressed: (){
+                        //todo authenticate
+                        Navigator.pushReplacement(context, MaterialPageRoute(
+                          builder: (context)=> HomePage()
+                        ));
+                      },
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
