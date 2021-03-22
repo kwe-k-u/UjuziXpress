0b93bc9a63cf54a4272e309dcfe449b7ce8965a7
Kweku <kwekuappiah11@gmail.com>
2021-03-22 15:14:53 +0000

    fixed button bar

--- lib/UI/screens/HistoryPage.dart
+++ lib/UI/screens/HistoryPage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ujuzi_xpress/UI/widgets/DeliveryListTile.dart';
 import 'package:ujuzi_xpress/utils/DeliveryLocation.dart';
@@ -13,6 +14,24 @@ class HistoryPage extends StatefulWidget {
 }
 
 class _HistoryPageState extends State<HistoryPage> {
+  // DeliveryStatus __status = DeliveryStatus.pending;
+  List<bool> selectedArray = [true, false, false];
+
+
+
+
+  ButtonStyle selectedStyle = ButtonStyle(
+    backgroundColor: MaterialStateProperty.all(Colors.deepPurple),
+
+  );
+  // ButtonStyle deSelectedStyle = ButtonStyle();
+
+
+  TextStyle selectedText = TextStyle(color: Colors.white);
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,31 +45,48 @@ class _HistoryPageState extends State<HistoryPage> {
         child: Column(
           children: [
 
-            ButtonBar(
+
+            Container(
+              margin: EdgeInsets.only(top: 8.0),
+              height: MediaQuery.of(context).size.height *0.05,
+              child: ToggleButtons(
+                borderWidth: 1.5,
+                isSelected: selectedArray,
+                color: Colors.black,
+                borderColor: Colors.black,
+                borderRadius: BorderRadius.all(Radius.circular(24.0)),
+                fillColor: Colors.deepPurple,
+                onPressed: (index){
+                  setState(() {
+                    selectedArray = [false, false, false];
+                    selectedArray[index] = true;
+                  });
+                },
                 children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    child: Text("Pending",
 
-              ElevatedButton(
-                child: Text("Pending"),
-                onPressed: (){
+                      textAlign: TextAlign.center,
+                      style: selectedArray.elementAt(0) ? selectedText : TextStyle(fontWeight: FontWeight.bold),),
+                  ),
 
-                },
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    child:  Text("completed",
+                      textAlign: TextAlign.center,
+                      style: selectedArray.elementAt(1) ? selectedText : TextStyle(fontWeight: FontWeight.bold),),
+                  ),
+
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    child: Text("Canceled",
+                      textAlign: TextAlign.center,
+                      style: selectedArray.elementAt(2) ? selectedText : TextStyle(fontWeight: FontWeight.bold),),
+                  ),
+                ],
               ),
-
-              ElevatedButton(
-                child: Text("completed"),
-                onPressed: (){
-
-                },
-              ),
-
-              ElevatedButton(
-                child: Text("Canceled"),
-                onPressed: (){
-
-                },
-              ),
-
-            ]),
+            ),
 
             Expanded(
               child: ListView.builder(
